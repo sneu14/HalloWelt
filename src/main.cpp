fb51b15be385262fdcc3e289acf465fecd69cf6a
sneu14 <mail@sebastian-neu.de>
2024-06-13 23:50:23 +0200

    Ergebnis des Workshops

--- src/main.cpp
+++ src/main.cpp
@@ -4,12 +4,33 @@
 #include <ESPAsyncWebServer.h>
 #include <ESPAsyncTCP.h>
 
+AsyncWebServer webserver(80);
+
+String processor(const String &var)
+{
+  if (var == "TIME")
+  {
+    return String(millis());
+  }
+
+  if (var == "LED"){
+    if(digitalRead(LED_BUILTIN)){
+      return "AN";
+    } else {
+      return "OFF";
+    }
+  }
+
+  return var;
+}
+
 void setup()
 {
   pinMode(LED_BUILTIN, OUTPUT);
   Serial.begin(115200);
 
-  if (SPIFFS.begin()){
+  if (SPIFFS.begin())
+  {
     Serial.println("SPIFFS mount OK");
   };
 
@@ -18,6 +39,22 @@ void setup()
   IPAddress IP = WiFi.softAPIP();
   Serial.print("AP IP address: ");
   Serial.println(IP);
+
+  webserver.on("/", HTTP_GET, [](AsyncWebServerRequest *request)
+               { request->send(SPIFFS, "/index.html", "text/html", false, processor); });
+
+  webserver.serveStatic("/", SPIFFS, "/");
+
+  webserver.on("/on", HTTP_GET, [](AsyncWebServerRequest *request)
+               {
+      digitalWrite(LED_BUILTIN, HIGH);
+      request->send(SPIFFS, "/index.html", "text/html", false, processor); });
+
+  webserver.on("/off", HTTP_GET, [](AsyncWebServerRequest *request)
+               {
+      digitalWrite(LED_BUILTIN, LOW);
+      request->send(SPIFFS, "/index.html", "text/html", false, processor); });
+  webserver.begin();
 }
 
 void loop()
